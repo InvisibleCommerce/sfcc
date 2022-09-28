4d9dadf5151c562a3b615ec1c671436a7ec72256
Steven Ou <steven@invisiblecommerce.com>
2022-09-28 11:43:00 -0700

    setting price sort of works - the price is in fact updated but certain other fields like tax basis and gross price are not adjusted correctly...

--- cartridges/shippedCartridge/cartridge/controllers/Checkout.js
+++ cartridges/shippedCartridge/cartridge/controllers/Checkout.js
@@ -10,23 +10,43 @@ server.prepend('Begin', server.middleware.get, function (req, res, next) {
   // res.json({ status: 'added', session: session });
 
   var BasketMgr = require('dw/order/BasketMgr');
+  var ProductMgr = require('dw/catalog/ProductMgr');
   var Transaction = require('dw/system/Transaction');
   var currentBasket = BasketMgr.getCurrentBasket();
   var totalPrice = currentBasket.merchandizeTotalGrossPrice.value;
   var cartHelper = require('*/cartridge/scripts/cart/cartHelpers');
   var shippedLi;
+  var product = ProductMgr.getProduct('shipped-shield');
 
-  Transaction.wrap(function () {
-    shippedLi = cartHelper.addProductToCart(currentBasket, 'shipped-shield', 1, [], []);
-  });
 
+  // remove any existing items first
+  // var existingLineItems = currentBasket.getAllProductLineItems();
+  // for each (var lineItem in existingLineItems.toArray()) {
+  //   if (lineItem.productID === 'shipped-shield') {
+  //     Transaction.wrap(function () {
+  //       currentBasket.removeProductLineItem(lineItem);
+  //     });
+  //   }
+  // }
+
+  // add relevant items
   Transaction.wrap(function () {
+    shippedLi = cartHelper.addLineItem(
+      currentBasket,
+      product,
+      1,
+      [],
+      product.getOptionModel(),
+      currentBasket.getDefaultShipment()
+    );
     shippedLi.setPriceValue(0.97);
-    shippedLi.setQuantityValue(1);
   });
+
 
   // if (true) { // should add or remove shipped?
-  //   res.json({ status: 'added', totalPrice: totalPrice });
+  //   res.json({
+  //     status: 'added', grossPrice: shippedLi.grossPrice.value, price: shippedLi.price.value, quantity: shippedLi.quantity.value
+  //   });
   // }
   next();
 });
