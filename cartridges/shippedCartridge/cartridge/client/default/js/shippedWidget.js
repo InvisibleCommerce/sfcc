43971c10d687b59008add808cb9ea4fd4270bf26
Steven Ou <steven@invisiblecommerce.com>
2022-10-03 18:38:43 -0700

    add custom preferences

--- cartridges/shippedCartridge/cartridge/client/default/js/shippedWidget.js
+++ cartridges/shippedCartridge/cartridge/client/default/js/shippedWidget.js
@@ -1,8 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const shield = new Shipped.Shield({
-    publicKey: 'pk_development_9570b140f70c9c69865eb67f6dea91d4efbbc5d03adc4924e91a9b4c766ddc30',
-    shieldDiv: '.shipped-shield-div'
-  });
+  const shield = new Shipped.Shield(shippedConfig);
 
   shield.updateOrderValue(430);
   shield.onChange(function(details) {
