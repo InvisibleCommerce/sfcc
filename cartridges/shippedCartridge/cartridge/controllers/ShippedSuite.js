b599459b02c16bfe681c33d87ab71092bcbb77e3
Steven Ou <steven@invisiblecommerce.com>
2022-10-05 19:26:19 -0700

    cart page also ensures basket has correct shipped line items

--- cartridges/shippedCartridge/cartridge/controllers/ShippedSuite.js
+++ cartridges/shippedCartridge/cartridge/controllers/ShippedSuite.js
@@ -1,8 +1,14 @@
 const server = require('server');
+var shippedBasketHelpers = require('~/cartridge/scripts/helpers/shippedBasketHelpers');
+var BasketMgr = require('dw/order/BasketMgr');
 
 server.post('Add', function (req, res, next) {
   // let's add a variable to session indicating user opted in
   req.session.privacyCache.set('shippedSuite', true);
+
+  var currentBasket = BasketMgr.getCurrentBasket();
+  shippedBasketHelpers.ensureCorrectShippedLineItems(currentBasket, true);
+
   res.json({ status: 'added', session: req.session.privacyCache.shippedSuite });
   next();
 });
@@ -10,6 +16,10 @@ server.post('Add', function (req, res, next) {
 server.post('Remove', function (req, res, next) {
   // let's add a variable to session indicating user opted out
   req.session.privacyCache.set('shippedSuite', false);
+
+  var currentBasket = BasketMgr.getCurrentBasket();
+  shippedBasketHelpers.ensureCorrectShippedLineItems(currentBasket, false);
+
   res.json({ status: 'removed', session: req.session.privacyCache.shippedSuite });
   next();
 });
