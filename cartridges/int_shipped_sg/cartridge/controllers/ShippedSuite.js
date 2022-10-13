142fc04506dd9e7a730b1f46ba27a8995289d0ac
Steven Ou <steven@invisiblecommerce.com>
2022-10-12 17:37:52 -0700

    remove unused code. add widget on checkout

--- cartridges/int_shipped_sg/cartridge/controllers/ShippedSuite.js
+++ cartridges/int_shipped_sg/cartridge/controllers/ShippedSuite.js
@@ -1,35 +1,10 @@
 'use strict';
 
-var app = require('*/cartridge/scripts/app');
 var guard = require('*/cartridge/scripts/guard');
 
 var shippedBasketHelpers = require('int_shipped/cartridge/scripts/helpers/shippedBasketHelpers');
 var BasketMgr = require('dw/order/BasketMgr');
 var Response = require('*/cartridge/scripts/util/Response');
-var Locale = require('dw/util/Locale');
-
-// function buildCheckoutResponse(currentBasket, req, res) {
-//   var currentCustomer = req.currentCustomer.raw;
-//   var usingMultiShipping = req.session.privacyCache.get('usingMultiShipping');
-//   var allValid = COHelpers.ensureValidShipments(currentBasket);
-//   var currentLocale = Locale.getLocale(req.locale.id);
-//   var accountModel = new AccountModel(req.currentCustomer);
-//
-//   COHelpers.recalculateBasket(currentBasket);
-//
-//   var orderModel = new OrderModel(
-//     currentBasket,
-//     {
-//       customer: currentCustomer,
-//       usingMultiShipping: usingMultiShipping,
-//       shippable: allValid,
-//       countryCode: currentLocale.country,
-//       containerView: 'basket'
-//     }
-//   );
-//
-//   res.json({ order: orderModel, customer: accountModel });
-// }
 
 function add() {
   // let's add a variable to session indicating user opted in
@@ -38,7 +13,6 @@ function add() {
   var currentBasket = BasketMgr.getCurrentBasket();
   shippedBasketHelpers.ensureCorrectShippedLineItems(currentBasket, true);
 
-  // buildCheckoutResponse(currentBasket);
   Response.renderJSON({
     success: true
   });
@@ -51,7 +25,6 @@ function remove() {
   var currentBasket = BasketMgr.getCurrentBasket();
   shippedBasketHelpers.ensureCorrectShippedLineItems(currentBasket, false);
 
-  // buildCheckoutResponse(currentBasket);
   Response.renderJSON({
     success: true
   });
