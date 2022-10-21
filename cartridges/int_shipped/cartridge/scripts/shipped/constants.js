1472b549b2627fb8dd1995a5dc70223ad1d543d0
Steven Ou <steven@invisiblecommerce.com>
2022-10-22 07:02:24 +0800

    use resource strings for easier localization. resolves #20

--- cartridges/int_shipped/cartridge/scripts/shipped/constants.js
+++ cartridges/int_shipped/cartridge/scripts/shipped/constants.js
@@ -1,10 +1,12 @@
 'use strict';
 
+var Resource = require('dw/web/Resource');
+
 var shipped_shield_id = 'shipped-shield';
 var shipped_green_id = 'shipped-green';
 var shipped_descriptions = {};
-shipped_descriptions[shipped_shield_id] = 'Shipped Shield';
-shipped_descriptions[shipped_green_id] = 'Shipped Green';
+shipped_descriptions[shipped_shield_id] = Resource.msg('label.shipped.shield', 'shipped', 'Shipped Shield');
+shipped_descriptions[shipped_green_id] = Resource.msg('label.shipped.green', 'shipped', 'Shipped Green');
 
 exports.SHIPPED_IDS = [shipped_shield_id, shipped_green_id];
 exports.SHIPPED_SHIELD_ID = shipped_shield_id;
