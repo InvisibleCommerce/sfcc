5be03986b514e5ee1c8a0ef436a7f8ddc2fb7d7a
Steven Ou <steven@invisiblecommerce.com>
2022-10-21 17:56:57 +0800

    WIP JSDoc

--- cartridges/int_shipped/cartridge/client/js/shippedWidgetHelpers.js
+++ cartridges/int_shipped/cartridge/client/js/shippedWidgetHelpers.js
@@ -2,11 +2,41 @@
 
 var existingDetails;
 
+/**
+ * @description Parses out the subtotal number from a subtotal string
+ * @param {string} subtotal The subtotal string
+ * @returns {number}
+ */
+
 function subtotalValue(subtotal) {
   if (subtotal === null || subtotal === undefined) return 0;
 
   return parseFloat(subtotal.replace('$', '').replace(',', ''));
 }
+
+/**
+ * shouldRefreshUI callback function containing logic to determine
+ * whether the UI needs to be refreshed
+ * @callback shouldRefreshUI
+ * @param {object} existingDetails Existing/previous response object from Shipped Suite Offers API
+ * @param {object} details Latest response objects from Shipped Suite Offers API
+ * @param {HTMLElement} widget The <div> node for the Shipped Widget
+ * @returns {boolean}
+ */
+
+/**
+ * refreshUI callback function to execute UI refresh
+ * @callback refreshUI
+ * @param {object} data Contains the latest cart object
+ */
+
+/**
+ * @description Callback handler for Shipped widget - calls further handlers for UI refresh logic
+ * @param widget {HTMLElement} The <div> node for the Shipped Widget
+ * @param details {object} Response object from Shipped Suite Offers API
+ * @param shouldRefreshUI {shouldRefreshUI}
+ * @param refreshUI {refreshUI}
+ */
 
 function handleShippedChange(widget, details, shouldRefreshUI, refreshUI) {
   let path;
@@ -35,6 +65,12 @@ function handleShippedChange(widget, details, shouldRefreshUI, refreshUI) {
     }
   });
 }
+
+/**
+ * @description Initializes the Shipped Widget
+ * @param shouldRefreshUI {shouldRefreshUI}
+ * @param refreshUI {refreshUI}
+ */
 
 function initializeWidget(shouldRefreshUI, refreshUI) {
   const shippedWidget = new Shipped.Widget(shippedConfig);
